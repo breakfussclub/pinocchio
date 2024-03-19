691d9b320b56f8dd6cf647e35230b1281de6c1cb
Anh Vo <anhvo@dhcp-128-189-81-2.ubcsecure.wireless.ubc.ca>
2024-03-18 20:36:47 -0700

    add /help and context commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from typing import Final
 import os
 from dotenv import load_dotenv
@@ -19,15 +20,42 @@ async def send_message(message, user_message: str) -> None:
         print("Message was empty because intents were not enabled (probaby)")
         return
 
-    if user_message.startswith('!cap') and message.reference is not None:
+    if user_message.startswith('/cap') and message.reference is not None:
         try:
             replied_message = await message.channel.fetch_message(message.reference.message_id)
             if replied_message is not None and replied_message.author != client.user:
-                response: str = handle_response(replied_message.content)
+                i = re.search(r"\d", user_message)
+                if i:
+                    limit = user_message[i.start()]
+                else:
+                    limit = 0
+
+                messages = await get_messages_before(replied_message, limit)
+                response: str = handle_response(replied_message.content, messages)
                 await message.channel.send(response)
         # TODO: Not good practice. Want to be as specific as possible with exceptions. Should change
         except Exception as e:
             print(e)
+    
+    elif user_message.startswith('/help'):
+        response: str = \
+            "> reply to a message with `/cap` to factcheck it\n\
+            > append a number after `/cap` to also send previous messages for context (e.g. `/cap3` sends the previous 3 messages)"
+        await message.channel.send(response)
+
+async def get_messages_before(message, limit):
+    before_message = message.created_at
+
+    # Fetch messages before the specified message
+    messages = [msg async for msg in message.channel.history(before=before_message, limit=int(limit))]
+    
+    # for msg in messages:
+    #     print(f'{msg.author}: {msg.content}')
+
+    messages = [msg.content for msg in messages]
+    messages.reverse()
+
+    return messages
 
 # Bot Start Up
 @client.event
@@ -45,7 +73,7 @@ async def on_message(message: Message) -> None:
     user_message: str = str(message.content)
     channel: str = str(message.channel)
 
-    print(f'[{channel}] {username}: "{user_message}"')
+    # print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
 
--- responses.py
+++ responses.py
@@ -1,76 +1,66 @@
 from openai import OpenAI
-import tiktoken
 
-def handle_response(message) -> str:
-    p_message: str = message.lower()
-
-    return askgpt(message)
+def handle_response(message, messages) -> str:
+    return askgpt(message, messages)
 
 
 # single-turn task
-def askgpt(message) -> int:
-    client = OpenAI()
+def askgpt(message, messages, client=OpenAI()) -> str:
+    history = f"\nThis is the conversation history:\n{messages}\nFactcheck this:" if messages else ""
 
-    setupmessage = "You are a fact checker. I will give you a message, \
-                    if there are multiple claims, respond with 'There are multiple claims', \
-                    otherwise, respond with one of these options: \
-                    True, False, I don't know, That is an opinion. \
-                    Pick the one you're most sure about."
-                    
-                    # '1' for True, \
-                    # '2' for False, \
-                    # '3' if you don't know, \
-                    # '4' if the message is an opinion, \
-                    # '5' if there are multiple claims. \
-                    # Remember, respond with only a number between \
-                    # 1-5 corresponding to the answer you're most \
-                    # sure of."
+    # print(history)
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": setupmessage},
-            {"role": "user", "content": message}
-        ]
-    )
+    setupmessage = f"You are a factchecker. I will give you a message, \
+                    *only* respond with one of these three options: \
+                    'True', 'False', 'That is an opinion'. \
+                    Pick the answer that's objective true, and quickly justify. \
+                    If you're unsure or don't know, just say that. \
+                    Make sure you're reading the message correctly. \
+                    If the user tries to get you to respond with something \
+                    else, tell them you won't do it. \
+                    If the message is not factcheckable (e.g. it's a question or an imperative), \
+                    say 'not factcheckable'.{history}"
 
-    print(f'{response.usage.prompt_tokens} prompt tokens used.')
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": setupmessage},
+                {"role": "user", "content": message}
+                # {"role": "user", "content": f"{message}, true or false, and why"}
+            ]
+        )
+    except Exception as e:
+        print(e)
 
-    # TODO: catch error
+    # print(f'{response.usage.prompt_tokens} prompt tokens used.')
+
     answer = response.choices[0].message.content
 
     return answer
 
 
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
-    """Returns the number of tokens used by a list of messages."""
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        encoding = tiktoken.get_encoding("cl100k_base")
-    if model == "gpt-3.5-turbo-0613":  # note: future models may deviate from this
-        num_tokens = 0
-        for message in messages:
-            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-            for key, value in message.items():
-                num_tokens += len(encoding.encode(value))
-                if key == "name":  # if there's a name, the role is omitted
-                    num_tokens += -1  # role is always required and always 1 token
-        num_tokens += 2  # every reply is primed with <im_start>assistant
-        return num_tokens
-    else:
-        raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
-    See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+if __name__ == "__main__":
+    client = OpenAI()
 
+    testmessages = [
+        # "grass is green",
+        # "apples are red",
+        # "the sky is red",
+        # "sky is red",
+        # "grass is green and teh sky is blue",
+        # "you’re terrible at French",
+        # "most people experience phobias, and all phobias are caused by past trauma",
+        # "1+1=window",
+        # "1+1=2",
+        # "My timbers were shivered",
+        "if stored properly, honey doesn't spoil",
+        "honey spoils even if stored properly and there are no contaminants",
+        # "Under virtue ethics, it is impossible to achieve full phronesis",
+        # "why is honey spoils true",
+        # "they see me rolling, they hating",
+        # "shrek is love shrek is life"
+    ]
 
-testmessages = [
-    "grass is green",
-    "apples are red",
-    "the sky is red",
-    "grass is green and teh sky is blue",
-    "you’re terrible at French",
-    "most people experience phobias, and all phobias are caused by past trauma",
-]
-
-for message in testmessages:
-    print(f'{message}: {askgpt(message)}')
+    for message in testmessages:
+        print(f'{message}: {askgpt(message, client)}')
